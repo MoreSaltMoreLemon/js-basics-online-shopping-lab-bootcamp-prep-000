e17ee7411c829533842feaf0780c8145f871650a
ezraschwepker@gmail.com <ezraschwepker@gmail.com>
2018-09-10 17:04:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,11 +51,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber > 0) {
-    
-  } else {
+  if (cardNumber !== undefined) {
     let totalCart = total();
     cart = [];
     return "Your total cost is $" + totalCart + ", which will be charged to the card " + cardNumber + ".";
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
   }
 }
