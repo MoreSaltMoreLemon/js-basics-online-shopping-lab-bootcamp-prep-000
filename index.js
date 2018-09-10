c134626cc3a79b292268d93ee12bfe914526e1d7
ezraschwepker@gmail.com <ezraschwepker@gmail.com>
2018-09-10 17:00:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === "" || null || undefined || NaN) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    let total = total();
+    let totalCart = total();
     cart = [];
-    return "Your total cost is $" + total + "which will be charged to the card " + cardNumber + " .";
+    return "Your total cost is $" + totalCart + "which will be charged to the card " + cardNumber + " .";
   }
 }
